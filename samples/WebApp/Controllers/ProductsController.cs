412e4ec355413bfaa450ab469d846ad4a7ad1082
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-03-11 23:55:39 -0800

    Create sample WebApp project

---
+++ samples/WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [RoutePrefix("products")]
+    public class ProductsController : ApiController
+    {
+        readonly Product[] _products = {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        [Route("")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = _products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ samples/WebApp/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApp
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(Register);
+        }
+
+        private static void Register(HttpConfiguration config)
+        {
+            // disable XML media types and formatter
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            // use ISO8601Literal date time formatting
+            config.Formatters.JsonFormatter.SerializerSettings.DateFormatString = "yyyy-MM-ddTHH:mm:ssZ";
+
+            // serialize enum as its string representation
+            config.Formatters.JsonFormatter.SerializerSettings.Converters
+                .Add(new StringEnumConverter());
+
+            // setup routing
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
