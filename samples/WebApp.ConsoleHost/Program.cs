b1852e3cd4f24e36c73a8471cbff9bf9e5c7a4f0
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-03-19 16:31:25 -0700

    Add sample WebApp and corresponding Azure Function to host it:

---
+++ samples/WebApp.ConsoleHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace WebApp.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var handler = new HttpClientHandler();
+        }
+    }
+}
