900300c9521a9b45c24ef2e99dfa41a5c2620906
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-03-19 18:33:12 -0700

    Define IFunction and a sample function project

---
+++ samples/SampleFunction/AFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFunction
+{
+    public class AFunction : IFunction
+    {
+        private readonly string _queueMessage;
+
+        public AFunction([QueueTrigger("sample_queue")] string queueMessage)
+        {
+            _queueMessage = queueMessage;
+        }
+
+        public void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Servernet/IFunction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servernet
+{
+    public interface IFunction
+    {
+        void Run();
+    }
+}
