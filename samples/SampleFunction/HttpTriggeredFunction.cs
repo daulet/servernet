15d54a2f1dab410791204e612aa930791d5602c1
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-03-20 00:10:51 -0700

    Add HttpTrigger

---
+++ samples/SampleFunction/HttpTriggeredFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using SampleFunction.Model;
+using Servernet;
+
+namespace SampleFunction
+{
+    public class HttpTriggeredFunction : IFunction<HttpResponseMessage>
+    {
+        private readonly HttpRequestMessage _request;
+
+        public HttpTriggeredFunction([HttpTrigger] HttpRequestMessage request)
+        {
+            _request = request;
+        }
+
+        public HttpResponseMessage Run()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ samples/SampleFunction/Model/Purchase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Servernet;
+
+namespace SampleFunction.Model
+{
+    [Serializer]
+    public class Purchase
+    {
+        public double Amount { get; set; }
+
+        public string CustomerName { get; set; }
+    }
+}
