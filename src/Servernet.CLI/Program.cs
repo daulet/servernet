e074728c2375946716956a396ae8dc21cca5576a
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-03-29 22:22:42 -0700

    Prepare Program.Run for generating multiple Functions

--- src/Servernet.CLI/Program.cs
+++ src/Servernet.CLI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace Servernet.CLI
 {
@@ -18,45 +19,54 @@ namespace Servernet.CLI
         private void Run()
         {
             var assemblyPath = Path.Combine(Environment.CurrentDirectory, _options.Assembly);
+            var foundFunctions = new HashSet<Tuple<Type, MethodInfo>>();
 
-            var fullFunctionName = _options.Function;
-            if (string.IsNullOrEmpty(fullFunctionName))
+            if (string.IsNullOrEmpty(_options.Function))
             {
-                Console.WriteLine("Function name can't be empty");
-                return;
+                // find all [AzureFunction] attributes
             }
-            // assuming function name can't contain a period
-            var indexOfLastPeriod = fullFunctionName.LastIndexOf(".", StringComparison.OrdinalIgnoreCase);
-            if (indexOfLastPeriod <= 0)
+            else
             {
-                Console.WriteLine($"Invalid fully qualified method name: {fullFunctionName}");
-                return;
+                var fullFunctionName = _options.Function;
+                // assuming function name can't contain a period
+                var indexOfLastPeriod = fullFunctionName.LastIndexOf(".", StringComparison.OrdinalIgnoreCase);
+                if (indexOfLastPeriod <= 0)
+                {
+                    Console.WriteLine($"Invalid fully qualified method name: {fullFunctionName}");
+                    return;
+                }
+                var typeName = fullFunctionName.Substring(0, indexOfLastPeriod);
+                var methodName = fullFunctionName.Substring(indexOfLastPeriod + 1);
+
+                // Locate a method
+
+                var locator = new MethodLocator();
+                var locatedMethod = locator.Locate(assemblyPath, typeName, methodName);
+
+                foundFunctions.Add(locatedMethod);
             }
-            var typeName = fullFunctionName.Substring(0, indexOfLastPeriod);
-            var methodName = fullFunctionName.Substring(indexOfLastPeriod + 1);
 
-            // Locate a method
+            foreach (var function in foundFunctions)
+            {
+                var functionType = function.Item1;
+                var functionMethod = function.Item2;
 
-            var locator = new MethodLocator();
-            var locatedMethod = locator.Locate(assemblyPath, typeName, methodName);
-            var functionType = locatedMethod.Item1;
-            var functionMethod = locatedMethod.Item2;
+                // Generate bindings
 
-            // Generate bindings
+                var parser = new AttributeParser();
+                var functionBuilder = parser.ParseEntryPoint(functionType, functionMethod);
+                functionBuilder.Validate(_log);
 
-            var parser = new AttributeParser();
-            var functionBuilder = parser.ParseEntryPoint(functionType, functionMethod);
-            functionBuilder.Validate(_log);
+                // Generate output
 
-            // Generate output
-
-            var outputDirectory = string.IsNullOrEmpty(_options.OutputDirectory)
-                ? functionType.Name
-                : _options.OutputDirectory;
-            var sourceDirectory = new FileInfo(functionType.Assembly.Location).Directory;
-            var targetDirectory = new DirectoryInfo(outputDirectory);
-            var releaseBuilder = new ReleaseBuilder();
-            releaseBuilder.Release(sourceDirectory, targetDirectory, functionBuilder);
+                var outputDirectory = string.IsNullOrEmpty(_options.OutputDirectory)
+                    ? functionType.Name
+                    : _options.OutputDirectory;
+                var sourceDirectory = new FileInfo(functionType.Assembly.Location).Directory;
+                var targetDirectory = new DirectoryInfo(outputDirectory);
+                var releaseBuilder = new ReleaseBuilder();
+                releaseBuilder.Release(sourceDirectory, targetDirectory, functionBuilder);
+            }
         }
     }
 }
