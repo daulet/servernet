b028e6d7313f87e3207253967efad242ffc24ae0
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-03-30 03:15:38 -0700

    Add SoapHandler sample boilerplate

---
+++ samples/SoapEndpointSample/SoapHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Servernet.Samples.SoapEndpointSample
+{
+    [AzureFunction]
+    public class SoapHandler
+    {
+        [HttpResponse]
+        public static async Task<HttpResponseMessage> RunAsync(
+            [HttpTrigger(HttpMethod.Get | HttpMethod.Post, "ols/olsclient.svc/olsclient")] HttpRequestMessage request,
+            TraceWriter logger)
+        {
+            var content = await request.Content.ReadAsStringAsync();
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(content),
+            };
+        }
+    }
+}
