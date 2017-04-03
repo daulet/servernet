bcda02fbae3e2c2925f1e4c325cd6cd1a03e6b6d
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-04-02 23:01:41 -0700

    Add HTTP Trigger sample

---
+++ samples/DocumentationSamples/HttpTriggerFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Servernet.Samples.DocumentationSamples
+{
+    [AzureFunction]
+    public class HttpTriggerFunction
+    {
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(HttpMethod.Get, "HttpTriggerFunction")] HttpRequestMessage req,
+            TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function processed a request. RequestUri={req.RequestUri}");
+
+            // parse query parameter
+            var name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", StringComparison.OrdinalIgnoreCase) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+        }
+    }
+}
