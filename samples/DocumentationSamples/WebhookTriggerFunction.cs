92488afe47957290893925071aac5d07ba0505ae
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-04-02 21:21:26 -0700

    Add WebhookTriggerFunction sample

---
+++ samples/DocumentationSamples/WebhookTriggerFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Servernet.Samples.DocumentationSamples
+{
+    [AzureFunction]
+    public class WebhookTriggerFunction
+    {
+        [HttpResponse]
+        public static HttpResponseMessage Run(
+            [WebhookTrigger("products/{category:alpha}/{id:int?}", WebhookType.GenericJson)] HttpRequestMessage request,
+            string category,
+            int? id)
+        {
+            if (id == null)
+                return request.CreateResponse(HttpStatusCode.OK, $"All {category} items were requested.");
+            else
+                return request.CreateResponse(HttpStatusCode.OK, $"{category} item with id = {id} has been requested.");
+        }
+    }
+}
