f357ddb789345b90b41b9812150364cc3c19873f
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-04-03 22:02:19 -0700

    Add Table Input and Output samples

---
+++ samples/DocumentationSamples/TableInputFunction.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Servernet.Samples.DocumentationSamples
+{
+    [AzureFunction]
+    public class TableInputFunction
+    {
+        public static void Run(
+            [QueueTrigger("myqueue-items")] string myQueueItem,
+            [Table("Person", "Test", "{queueTrigger}")] Person personEntity,
+            TraceWriter log)
+        {
+            log.Info($"C# Queue trigger function processed: {myQueueItem}");
+            log.Info($"Name in Person entity: {personEntity.Name}");
+        }
+
+        public class Person
+        {
+            public string PartitionKey { get; set; }
+            public string RowKey { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ samples/DocumentationSamples/TableOutputFunction.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Servernet.Samples.DocumentationSamples
+{
+    [AzureFunction]
+    public class TableOutputFunction
+    {
+        public static void Run(
+            [QueueTrigger("myqueue-items")]string input,
+            [Table("Person")] ICollector<Person> tableBinding,
+            TraceWriter log)
+        {
+            for (var i = 1; i < 10; i++)
+            {
+                log.Info($"Adding Person entity {i}");
+                tableBinding.Add(
+                    new Person()
+                    {
+                        PartitionKey = "Test",
+                        RowKey = i.ToString(),
+                        Name = "Name" + i
+                    }
+                );
+            }
+        }
+
+        public class Person
+        {
+            public string PartitionKey { get; set; }
+            public string RowKey { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
