01dbf6b946a569d0578b259a25f2e2ee07131012
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-07-19 10:34:33 -1000

    Add BlobInputOutputFunction tests

---
+++ samples/DocumentationSamples.UnitTests/BlobInputOutputFunctionTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Moq;
+using Servernet.Generator;
+using Xunit;
+
+namespace Servernet.Samples.DocumentationSamples.UnitTests
+{
+    public class BlobInputOutputFunctionTests
+    {
+        [Fact]
+        public void Generate_FunctionJson_ExpectedOutput()
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var textWriter = new StreamWriter(memoryStream))
+                {
+                    var assemblyLoader = new Mock<IAssemblyLoader>();
+                    assemblyLoader
+                        .Setup(x => x.LoadFrom(It.IsAny<string>()))
+                        .Returns(typeof(BlobInputOutputFunction).Assembly);
+
+                    var environment = new Mock<IEnvironment>();
+                    environment
+                        .Setup(x => x.CurrentDirectory)
+                        .Returns(@"D:\fake\root\directory");
+
+                    var fileSystem = new Mock<IFileSystem>();
+                    fileSystem
+                        .Setup(x => x.CreateFileWriter(It.IsAny<string>()))
+                        .Returns(textWriter);
+
+                    var options = new Options()
+                    {
+                        AssemblyPath = string.Empty,
+                        Function = "Servernet.Samples.DocumentationSamples.BlobInputOutputFunction.Run",
+                    };
+
+                    var program = new Program(
+                        assemblyLoader.Object,
+                        environment.Object,
+                        fileSystem.Object,
+                        Mock.Of<ILogger>(),
+                        options);
+
+                    program.Run();
+
+                    fileSystem.VerifyAll();
+
+                    memoryStream.Flush();
+                    memoryStream.Position = 0;
+                    using (var streamReader = new StreamReader(memoryStream))
+                    {
+                        var functionDef = streamReader.ReadToEnd();
+
+                        Console.WriteLine(functionDef);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ samples/DocumentationSamples.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DocumentationSamples.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DocumentationSamples.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5eff2d84-d474-4739-a0a8-5b48062a0443")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
