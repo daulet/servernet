34f9d271b208268bce380c27dc64a0d2a93eb785
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-07-25 17:39:54 -0700

    Add new Servernet.Azure.KeyVault extension library

---
+++ src/Servernet.Azure.KeyVault/SecretAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Azure.WebJobs.Description;
+
+namespace Servernet.Azure.KeyVault
+{
+    [Binding]
+    public class SecretAttribute : Attribute
+    {
+        
+    }
+}
